124536dbded7ef0889d49a9c0cd2259e0601ecb6
Sakshi4480 <112326688+Sakshi4480@users.noreply.github.com>
2024-09-29 01:49:15 +0530

    first commit

---
+++ react-portfolio-main/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:sakshipandey4480@gmail.com">sakshipandey4480@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/sakshi-pandey-a3708a228">linkedin.com/sakshipandey</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Sakshi4480">github.com/Sakshi4480</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
